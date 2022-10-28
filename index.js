f905bcd19f6c0fbbcf57a35c5aa2133cd3bc75e2
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-10-28 17:53:30 +0600

    categories server added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT  || 5000;
 const cors = require('cors')
+const categories = require('./data/categories.json')
 
 
 app.use(cors())
@@ -9,6 +10,10 @@ app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('we love Allah...')
+})
+
+app.get('/categories', (req, res) => {
+  res.send(categories)
 })
 
 
