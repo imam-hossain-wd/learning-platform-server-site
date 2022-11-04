bc6fc5e91e0787493a6ce0b2a0d9054ce9fd10b5
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-11-04 16:54:08 +0600

    modify data

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT  || 5000;
 const cors = require('cors')
-const categories = require('./data/categories.json')
+// const categories = require('./data/categories.json')
 const courses = require('./data/courses.json')
 
 app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send(categories)
-})
 
 app.get('/courses', (req, res) => {
   res.send(courses)
